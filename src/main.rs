9e6c18171247479bf12cacf93e9235c044c6649c
a-rustacean <dilshadplayingminecraft@outlook.com>
2023-12-28 22:01:54 +0530

    style: format

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
-use std::io::Write;
+use chrono::{offset::Local, Datelike, Duration, NaiveDate, Weekday};
 use colored::Colorize;
-
-use chrono::{offset::Local, Datelike, NaiveDate, Weekday, Duration};
+use std::io::Write;
 
 const MONTHS: [&str; 12] = [
     "January",
@@ -43,19 +42,21 @@ fn month_to_string(month: u32) -> String {
 }
 
 pub fn get_days_from_month(year: i32, month: u32) -> Option<i64> {
-    Some(NaiveDate::from_ymd_opt(
-        match month {
-            12 => year + 1,
-            _ => year,
-        },
-        match month {
-            12 => 1,
-            _ => month + 1,
-        },
-        1,
-    )?
-    .signed_duration_since(NaiveDate::from_ymd_opt(year, month, 1)?)
-    .num_days())
+    Some(
+        NaiveDate::from_ymd_opt(
+            match month {
+                12 => year + 1,
+                _ => year,
+            },
+            match month {
+                12 => 1,
+                _ => month + 1,
+            },
+            1,
+        )?
+        .signed_duration_since(NaiveDate::from_ymd_opt(year, month, 1)?)
+        .num_days(),
+    )
 }
 
 fn main() {
@@ -75,17 +76,25 @@ fn main() {
     let month_start = current_date - Duration::days(current_date.day0() as i64);
     println!("Su Mo Tu We Th Fr Sa");
     let from_sunday = month_start.weekday().number_from_sunday() - 1;
-    print!("{} ", repeat(" ", 2 * from_sunday as usize + (from_sunday.max(1) as usize - 1)));
-    for day in 0..get_days_from_month(current_date.year(), current_date.month()).unwrap() {
+    print!(
+        "{} ",
+        repeat(
+            " ",
+            2 * from_sunday as usize + (from_sunday.max(1) as usize - 1)
+        )
+    );
+    for day in 0..get_days_from_month(current_date.year(), current_date.month())
+        .unwrap()
+    {
         if day as u32 == current_date.day0() {
             print!("{} ", pad((day + 1).to_string(), 2).black().on_white());
         } else {
-            print!("{} ", pad((day + 1).to_string(), 2)); 
+            print!("{} ", pad((day + 1).to_string(), 2));
         }
         std::io::stdout().flush().unwrap();
         if (month_start + Duration::days(day)).weekday() == Weekday::Sat {
             println!();
         }
-    };
+    }
     println!();
 }
